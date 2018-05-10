b15455abdc43a0914ad6b602f9f6629e877c8677
Ordan <ordan.santos@gmail.com>
2018-05-10 18:15:43 -0300

    Token creation

--- src/main/java/org/fogbowcloud/cli/CommandToken.java
+++ src/main/java/org/fogbowcloud/cli/CommandToken.java
@@ -1,16 +1,20 @@
 package org.fogbowcloud.cli;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.security.Identity;
+import java.security.InvalidParameterException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
 import org.fogbowcloud.manager.core.plugins.IdentityPlugin;
+import org.fogbowcloud.manager.core.plugins.identity.exceptions.TokenCreationException;
+import org.fogbowcloud.manager.core.plugins.identity.exceptions.UnauthorizedException;
 import org.reflections.Reflections;
 import org.reflections.scanners.SubTypesScanner;
 import org.reflections.util.ClasspathHelper;
 
+import com.beust.jcommander.DynamicParameter;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 
@@ -19,40 +23,52 @@ public class CommandToken {
 
 	private static final String PLUGIN_PACKAGE = "org.fogbowcloud.manager.core.plugins.identity";
 
-	@Parameter(names = { "--create", "-c" }, description = "Create a new token")
+	@Parameter(names = { "--create", "-c" }, description = "Create a new token", required = true)
 	private Boolean isCreation = false;
 
-	@Parameter(names = { "--type", "-t" }, description = "Identity plugin type")
-	private String identityPluginType;
+	@Parameter(names = { "--type", "-t" }, description = "Identity plugin type", required = true)
+	private String identityPluginType = null;
+	
+	@DynamicParameter(names = "-D", description = "Dynamic parameters")
+	Map<String, String> credentials = new HashMap<String, String>();
 
-	public void handle() throws InstantiationException, IllegalAccessException, IllegalArgumentException,
-			InvocationTargetException, NoSuchMethodException, SecurityException {
-
+	public String run() throws ReflectiveOperationException, UnauthorizedException, TokenCreationException {
 		if (isCreation) {
-			IdentityPlugin identityPlugin = getIdentityPluginByTypeName("ldap");
-			System.out.println(identityPlugin);
+			return createToken();
 		}
-
+		return null;
 	}
 
-	private IdentityPlugin getIdentityPluginByTypeName(String name)
-			throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException,
-			NoSuchMethodException, SecurityException {
-
+	private Class<? extends IdentityPlugin> getIdentityPluginClassByTypeName(String typeName) {
 		Reflections reflections = new Reflections(ClasspathHelper.forPackage(PLUGIN_PACKAGE), new SubTypesScanner());
 		Set<Class<? extends IdentityPlugin>> allClasses = reflections.getSubTypesOf(IdentityPlugin.class);
 
-		IdentityPlugin identityPlugin = null;
-
 		for (Class<? extends IdentityPlugin> currentClass : allClasses) {
-			if (currentClass.getName().contains(name)) {
-				System.out.println(currentClass);
-				Constructor<?> cons = currentClass.getConstructor(Properties.class);
-				identityPlugin = (IdentityPlugin) cons.newInstance(new Properties());
-				
+			if (currentClass.getName().contains(typeName)) {
+				return currentClass;
 			}
 		}
 
+		throw new InvalidParameterException("Token type [" + typeName + "] is not valid.");
+	}
+	
+	private IdentityPlugin getIdentityPlugin() throws ReflectiveOperationException {
+		Class<? extends IdentityPlugin> identityPluginClass = getIdentityPluginClassByTypeName(this.identityPluginType);
+		Constructor<?> constructor = identityPluginClass.getConstructor(Properties.class);
+		IdentityPlugin identityPlugin = (IdentityPlugin) constructor.newInstance(new Properties());
 		return identityPlugin;
+	}
+	
+	private String createToken() throws ReflectiveOperationException, UnauthorizedException, TokenCreationException {
+		
+		IdentityPlugin identityPlugin = getIdentityPlugin();
+		Map<String, String> userCredentials = new HashMap<String, String>();
+		
+		for (String key: this.credentials.keySet()) {
+			userCredentials.put(key, this.credentials.get(key));
+		}
+		
+		String accessId = identityPlugin.createToken(userCredentials).getAccessId();
+		return accessId;
 	}
 }
--- src/main/java/org/fogbowcloud/cli/Main.java
+++ src/main/java/org/fogbowcloud/cli/Main.java
@@ -1,13 +1,18 @@
 package org.fogbowcloud.cli;
 
-import java.lang.reflect.InvocationTargetException;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.fogbowcloud.manager.core.plugins.identity.exceptions.TokenCreationException;
+import org.fogbowcloud.manager.core.plugins.identity.exceptions.UnauthorizedException;
 
 import com.beust.jcommander.JCommander;
 
 public class Main {
 
-	public static void main(String[] args) throws InstantiationException, IllegalAccessException,
-			IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+	public static void main(String[] args) {
+
+		configureLog4j();
 
 		CommandToken commandToken = new CommandToken();
 
@@ -15,8 +20,22 @@ public class Main {
 		jCommander.parse(args);
 
 		if (jCommander.getParsedCommand().equals("token")) {
-			commandToken.handle();
+			try {
+				String output = commandToken.run();
+				System.out.println(output);
+			} catch (ReflectiveOperationException | UnauthorizedException | TokenCreationException e) {
+				System.out.println(e);
+				System.out.println(e.getMessage());
+				System.out.println(e.getCause());
+			}
 		}
 
+	}
+
+	private static void configureLog4j() {
+		ConsoleAppender console = new ConsoleAppender();
+		console.setThreshold(Level.OFF);
+		console.activateOptions();
+		Logger.getRootLogger().addAppender(console);
 	}
 }
