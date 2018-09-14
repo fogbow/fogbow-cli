8ddf2c86aae265dc38ff7a0e8b52a671f62d63a5
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-09-14 13:59:45 -0300

    Change implementation of command to validate token
    Make request to Fogbow RAS instead of instantiating identity plugin.

--- src/main/java/org/fogbowcloud/cli/authentication/token/CommandCheckToken.java
+++ src/main/java/org/fogbowcloud/cli/authentication/token/CommandCheckToken.java
@@ -1,15 +1,20 @@
 package org.fogbowcloud.cli.authentication.token;
 
+import com.beust.jcommander.ParameterException;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.fogbowcloud.cli.HttpUtil;
 import org.fogbowcloud.cli.authentication.CommandAuthentication;
-import org.fogbowcloud.manager.core.HomeDir;
-import org.fogbowcloud.manager.core.exceptions.TokenValueCreationException;
-import org.fogbowcloud.manager.core.plugins.behavior.federationidentity.FederationIdentityPlugin;
 
 import com.beust.jcommander.Parameter;
+import org.fogbowcloud.cli.constants.Messages;
+
+import java.io.IOException;
 
 public class CommandCheckToken extends CommandAuthentication {
 
     public static final String NAME = "check-token";
+    public static final String ENDPOINT = "tokens/validate";
 
     private static final String TOKEN_VALID = "Token Valid";
     private static final String TOKEN_INVALID = "Token Unauthorized";
@@ -17,15 +22,29 @@ public class CommandCheckToken extends CommandAuthentication {
     @Parameter(names = { "--federation-token-value", "-f" }, description = "Federation token value", required = true)
     private String federationTokenValue = null;
 
-    public String run() throws TokenValueCreationException, ReflectiveOperationException {
-        HomeDir.getInstance().setPath(getConfPath());
+    public static final String URL_COMMAND_KEY =  "--url";
+    @Parameter(names = { URL_COMMAND_KEY }, description = "Fogbow RAS URL", required = true)
+    protected String url = null;
+
+    public String run() throws IOException {
+        if (this.url == null || this.url.isEmpty()) {
+            throw new ParameterException(Messages.Exception.NO_MEMBERSHIP_SERVICE_URL);
+        }
+
+        if (this.federationTokenValue == null || this.federationTokenValue.isEmpty()) {
+            throw new ParameterException(Messages.Exception.NO_FEDERATION_TOKEN_PARAM);
+        }
+
         return checkToken();
     }
 
-    private String checkToken() throws TokenValueCreationException, ReflectiveOperationException {
-        FederationIdentityPlugin identityPlugin = getFederationIdentityPlugin();
+    // TODO: Currently this endpoints is not implemented in Fogbow RAS
+    private String checkToken() throws IOException {
 
-        boolean isValid = identityPlugin.isValid(this.federationTokenValue);
+        String fullUrl = this.url + ENDPOINT;
+        HttpResponse httpResponse = HttpUtil.get(fullUrl, this.federationTokenValue);
+        String isValidString = HttpUtil.getHttpEntityAsString(httpResponse);
+        boolean isValid = Boolean.valueOf(isValidString);
 
         if (isValid) {
             return TOKEN_VALID;
