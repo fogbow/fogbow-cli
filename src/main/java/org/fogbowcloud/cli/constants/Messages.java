3c62e9570d5734e1ed75318b7f093502ffa87d1c
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-09-14 11:24:51 -0300

    Create UnexpectedException

--- src/main/java/org/fogbowcloud/cli/constants/Messages.java
+++ src/main/java/org/fogbowcloud/cli/constants/Messages.java
@@ -1,4 +1,8 @@
 package org.fogbowcloud.cli.constants;
 
 public class Messages {
+
+    public static class Exception {
+        public static final String UNEXPECTED = "Unexpected exception.";
+    }
 }
--- src/main/java/org/fogbowcloud/cli/exceptions/UnexpectedException.java
+++ src/main/java/org/fogbowcloud/cli/exceptions/UnexpectedException.java
@@ -1,4 +1,19 @@
 package org.fogbowcloud.cli.exceptions;
 
-public class UnexpectedException {
+import org.fogbowcloud.cli.constants.Messages;
+
+public class UnexpectedException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public UnexpectedException() {
+        super(Messages.Exception.UNEXPECTED);
+    }
+
+    public UnexpectedException(String message) {
+        super(message);
+    }
+
+    public UnexpectedException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
