9833d745dca8046b6925b629cdec52b765504d59
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2018-09-14 13:53:21 -0300

    Change implementation of how getting info about member
    Make request for Fogbow RAS instead of instanciating identity plugin.

--- src/main/java/org/fogbowcloud/cli/authentication/user/CommandUser.java
+++ src/main/java/org/fogbowcloud/cli/authentication/user/CommandUser.java
@@ -1,21 +1,22 @@
 package org.fogbowcloud.cli.authentication.user;
 
+import com.beust.jcommander.ParameterException;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.fogbowcloud.cli.HttpUtil;
 import org.fogbowcloud.cli.authentication.CommandAuthentication;
-import org.fogbowcloud.manager.core.HomeDir;
-import org.fogbowcloud.manager.core.exceptions.TokenValueCreationException;
-import org.fogbowcloud.manager.core.exceptions.UnauthenticatedUserException;
-import org.fogbowcloud.manager.core.exceptions.UnexpectedException;
-import org.fogbowcloud.manager.core.models.tokens.FederationUser;
-import org.fogbowcloud.manager.core.plugins.behavior.federationidentity.FederationIdentityPlugin;
 
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
-import com.google.gson.Gson;
+import org.fogbowcloud.cli.constants.Messages;
+
+import java.io.IOException;
 
 @Parameters(separators = "=", commandDescription = "Token manipulation")
-public class CommandUser extends CommandAuthentication {
+public class CommandUser {
 
     public static final String NAME = "user";
+    public static final String ENDPOINT = "tokens/member";
 
     @Parameter(names = { "--get-user", "-g" }, description = "Get user information", required = true)
     private Boolean isGetUser = false;
@@ -23,28 +24,30 @@ public class CommandUser extends CommandAuthentication {
     @Parameter(names = { "--federation-token-value", "-f" }, description = "Federation token value", required = true)
     private String federationTokenValue = null;
 
-    public String run() throws ReflectiveOperationException, TokenValueCreationException, UnauthenticatedUserException, UnexpectedException {
+    public static final String URL_COMMAND_KEY =  "--url";
+    @Parameter(names = { URL_COMMAND_KEY }, description = "Fogbow RAS URL", required = true)
+    protected String url = null;
+
+    public String run() throws IOException {
+        if (this.url == null || this.url.isEmpty()) {
+            throw new ParameterException(Messages.Exception.NO_MEMBERSHIP_SERVICE_URL);
+        }
+
         if (this.isGetUser) {
-            HomeDir.getInstance().setPath(getConfPath());
-            FederationUser federationUser = getFederationUser();
-            return userToJson(federationUser);
+            return getFederationUser(this.federationTokenValue);
         }
 
         return null;
     }
 
-    private FederationUser getFederationUser()
-            throws TokenValueCreationException, ReflectiveOperationException, UnauthenticatedUserException, UnexpectedException {
+    // TODO: Currently this endpoints is not implemented in Fogbow RAS
+    private String getFederationUser(String federationTokenValue) throws IOException {
+        if (federationTokenValue == null || federationTokenValue.isEmpty()) {
+            throw new ParameterException(Messages.Exception.NO_FEDERATION_TOKEN_PARAM);
+        }
 
-        FederationIdentityPlugin identityPlugin = getFederationIdentityPlugin();
-        FederationUser federationUser = identityPlugin.getFederationUser(this.federationTokenValue);
-        return federationUser;
-    }
-
-    protected String userToJson(FederationUser federationUser) {
-        Gson gson = new Gson();
-        String jsonString = gson.toJson(federationUser);
-
-        return jsonString;
+        String fullUrl = this.url + ENDPOINT;
+        HttpResponse httpResponse = HttpUtil.get(fullUrl, this.federationTokenValue);
+        return HttpUtil.getHttpEntityAsString(httpResponse);
     }
 }
