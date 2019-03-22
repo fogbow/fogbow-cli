d29b52c77178d2dd9b27920e1dee990820d00133
Anderson Dantas <a.kleber.d@gmail.com>
2019-03-22 11:26:37 -0300

    Revise image command

--- src/main/java/cloud/fogbow/cli/ras/image/ImageCommand.java
+++ src/main/java/cloud/fogbow/cli/ras/image/ImageCommand.java
@@ -1,20 +1,17 @@
 package cloud.fogbow.cli.ras.image;
 
-import java.io.IOException;
-import java.util.Collections;
-
-import cloud.fogbow.cli.HttpUtil;
+import cloud.fogbow.cli.FogwbowHttpUtil;
 import cloud.fogbow.cli.constants.CliCommonParameters;
 import cloud.fogbow.cli.constants.Documentation;
 import cloud.fogbow.cli.constants.Messages;
 import cloud.fogbow.cli.exceptions.FogbowCLIException;
-import cloud.fogbow.cli.utils.CommandUtil;
-import org.apache.http.HttpResponse;
 
+import cloud.fogbow.common.exceptions.FogbowException;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.beust.jcommander.ParametersDelegate;
 
-public class ImageCommand {
+public class ImageCommand  {
 	public static final String NAME = "image";
 	public static final String ENDPOINT = '/' + "images";
 
@@ -24,15 +21,6 @@ public class ImageCommand {
 	@Parameter(names = CliCommonParameters.GET_ALL_COMMAND_KEY, description = Documentation.Image.GET_ALL)
 	protected Boolean isGetAllCommand = false;
 
-	@Parameter(names = CliCommonParameters.SYSTEM_USER_TOKEN_COMMAND_KEY, description = Documentation.CommonParameters.SYSTEM_USER_TOKEN)
-	private String systemUserToken = null;
-
-	@Parameter(names = CliCommonParameters.SYSTEM_USER_TOKEN_PATH_COMMAND_KEY, description = Documentation.CommonParameters.SYSTEM_USER_TOKEN_PATH)
-	private String systemUserTokenPath = null;
-
-	@Parameter(names = CliCommonParameters.URL_COMMAND_KEY, description = Documentation.CommonParameters.URL, required = true)
-	protected String url = null;
-	
 	@Parameter(names = CliCommonParameters.ID_COMMAND_KEY, description = Documentation.Image.ID)
 	protected String imageID = null;
 	
@@ -42,30 +30,37 @@ public class ImageCommand {
 	@Parameter(names = CliCommonParameters.CLOUD_NAME_COMMAND_KEY, description = Documentation.CommonParameters.CLOUD_NAME, required = true)
 	private String cloudName = null;
 
-	public String run() throws FogbowCLIException, IOException {
-		if (this.isGetCommand) {
-			return this.doGet();
-		} else if (this.isGetAllCommand) {
-			return this.doGetAll();
+	@ParametersDelegate
+	private FogwbowHttpUtil authenticatedRequest = new FogwbowHttpUtil();
+
+	public String run() throws FogbowCLIException{
+		try {
+			if (this.isGetCommand) {
+				return this.doGet();
+			} else if (this.isGetAllCommand) {
+				return this.doGetAll();
+			}
+		} catch (FogbowException e){
+			throw new FogbowCLIException(e.getMessage());
 		}
 		throw new ParameterException(Messages.Exception.INCOMPLETE_COMMAND);
 	}
-	
-	public String doGet() throws FogbowCLIException, IOException {
+
+	public String doGet() throws FogbowCLIException, FogbowException {
 		if (this.imageID == null) {
 			throw new ParameterException(Messages.Exception.NO_ID_INFORMED);
 		} else {
-			String fullUrl = this.url + ENDPOINT + "/" + this.memberId + "/" + this.cloudName + "/" + this.imageID;
-			String systemUserToken = CommandUtil.getSystemUserToken(this.systemUserToken, this.systemUserTokenPath);
-			HttpResponse httpResponse = HttpUtil.get(fullUrl, systemUserToken, Collections.emptyMap());
-			return HttpUtil.getHttpEntityAsString(httpResponse);
+			String path = ENDPOINT + "/" + this.memberId + "/" + this.cloudName + "/" + this.imageID;
+			String imageData = authenticatedRequest.doAuthenticatedGET(path);
+			return imageData;
 		}
 	}
-	
-	public String doGetAll() throws FogbowCLIException, IOException {
-		String fullUrl = this.url + ENDPOINT + "/" + this.memberId + "/" + this.cloudName;
-		String systemUserToken = CommandUtil.getSystemUserToken(this.systemUserToken, this.systemUserTokenPath);
-		HttpResponse httpResponse = HttpUtil.get(fullUrl, systemUserToken, Collections.emptyMap());
-		return HttpUtil.getHttpEntityAsString(httpResponse);
+
+	public String doGetAll() throws FogbowCLIException, FogbowException {
+		String path = ENDPOINT + "/" + this.memberId + "/" + this.cloudName;
+
+		String allImages = authenticatedRequest.doAuthenticatedGET(path);
+
+		return allImages;
 	}
 }
